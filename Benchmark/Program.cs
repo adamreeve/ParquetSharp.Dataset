f295c01f00dacb28552a8163d8a31a79ea1def5b
Adam Reeve <adam.reeve@gr-oss.io>
2025-07-01 12:00:44 +1200

    Add initial repro code

---
+++ Benchmark/Program.cs
@@ -0,0 +1,80 @@
+﻿using ParquetSharp;
+using ParquetSharp.Arrow;
+using ParquetSharp.Dataset;
+
+namespace Benchmark;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await ReadDataset(args[0], dateFilter: false, idFilter: true);
+        //await ReadWithArrow($"{args[0]}/test_data.parquet");
+        //await ReadWithPqs($"{args[0]}/test_data.parquet");
+    }
+
+    static async Task ReadDataset(string directoryPath, bool dateFilter = false, bool idFilter = false)
+    {
+        using var arrowProperties = ArrowReaderProperties.GetDefault();
+        //arrowProperties.PreBuffer = false;
+        var reader = new DatasetReader(directoryPath, arrowReaderProperties: arrowProperties);
+        IFilter? filter = null;
+        if (dateFilter)
+        {
+            filter = Col.Named("date").IsEqualTo(new DateOnly(2025, 1, 10));
+        }
+
+        if (idFilter)
+        {
+            var filterId = Col.Named("id").IsEqualTo(3);
+            filter = filter == null ? filterId : filter.And(filterId);
+        }
+
+        using var stream = reader.ToBatches(filter: filter);
+        var batchCount = 0;
+        while (await stream.ReadNextRecordBatchAsync() is { } batch)
+        {
+            batchCount++;
+            batch.Dispose();
+        }
+
+        Console.WriteLine($"Read {batchCount} batches");
+    }
+
+    static async Task ReadWithArrow(string filePath)
+    {
+        using var arrowProps = ArrowReaderProperties.GetDefault();
+        arrowProps.PreBuffer = false;
+        using var reader = new FileReader(filePath, arrowProperties: arrowProps);
+        using var batchReader = reader.GetRecordBatchReader();
+        var batchCount = 0;
+        while (await batchReader.ReadNextRecordBatchAsync() is { } batch)
+        {
+            batchCount++;
+            batch.Dispose();
+        }
+
+        Console.WriteLine($"Read {batchCount} batches");
+    }
+
+    static Task ReadWithPqs(string filePath)
+    {
+        using var reader = new ParquetFileReader(filePath);
+        var buffer = new float[1024 * 1024];
+        for (int rg = 0; rg < reader.FileMetaData.NumRowGroups; rg++)
+        {
+            using var rgReader = reader.RowGroup(rg);
+            for (int col = 2; col < rgReader.MetaData.NumColumns; col++)
+            {
+                var rowsRead = 0;
+                using var columnReader = rgReader.Column(col).LogicalReader<float>();
+                while (rowsRead < rgReader.MetaData.NumRows)
+                {
+                    rowsRead += columnReader.ReadBatch(buffer);
+                }
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
---
+++ GenerateData/Program.cs
@@ -0,0 +1,66 @@
+﻿using ParquetSharp;
+
+namespace GenerateData;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var columns = new List<Column>
+        {
+            new Column<DateOnly>("date"),
+            new Column<int>("id"),
+        };
+        const int numFloatCols = 451;
+        for (var i = 0; i < numFloatCols; ++i)
+        {
+            columns.Add(new Column<float>($"x{i}"));
+        }
+
+        using var writer = new ParquetFileWriter(args[0], columns.ToArray());
+
+        const int totalRows = 6_624_387;
+        const int rowsPerRowGroup = 1024*1024;
+        var date = new DateOnly(2025, 1, 1);
+
+        var dates = new DateOnly[rowsPerRowGroup];
+        var ids = new int[rowsPerRowGroup];
+        var x = new float[rowsPerRowGroup];
+        var rng = new Random(123);
+
+        var rowsWritten = 0;
+        var rowGroupIndex = 0;
+        while (rowsWritten < totalRows)
+        {
+            Console.WriteLine($"Row group {rowGroupIndex}");
+            var rowsToWrite = Math.Min(totalRows - rowsWritten, rowsPerRowGroup);
+            for (var i = 0; i < rowsToWrite; i++)
+            {
+                dates[i] = date;
+                ids[i] = i % 100;
+            }
+
+            using var rowGroup = writer.AppendRowGroup();
+            using var dateWriter = rowGroup.NextColumn().LogicalWriter<DateOnly>();
+            dateWriter.WriteBatch(dates.AsSpan(0, rowsToWrite));
+            using var idWriter = rowGroup.NextColumn().LogicalWriter<int>();
+            idWriter.WriteBatch(ids.AsSpan(0, rowsToWrite));
+            for (var colIdx = 0; colIdx < numFloatCols; colIdx++)
+            {
+                for (var i = 0; i < rowsToWrite; i++)
+                {
+                    x[i] = rng.NextSingle();
+                }
+
+                using var floatWriter = rowGroup.NextColumn().LogicalWriter<float>();
+                floatWriter.WriteBatch(x.AsSpan(0, rowsToWrite));
+            }
+
+            date = date.AddDays(1);
+            rowsWritten += rowsToWrite;
+            rowGroupIndex += 1;
+        }
+
+        writer.Close();
+    }
+}
